d5f8a212c45fe9c9a9f1b9f52e7aa3852b7a7e50
TimothyPandaRed <timothy@chewedleashphotography.com>
2024-06-11 22:20:16 +0100

    Refactor CSS and JavaScript - completed lighthouse report

--- main.js
+++ main.js
@@ -1,57 +1,24 @@
-const myH1 = document.querySelector("h1");
-const unpicked = document.getElementById("unpicked");
-const picked = document.getElementById("picked");
-const safeCampers = [];
-const campersPicked = document.getElementById("campersPicked");
-const li = document.createElement("li");
-campersPicked.appendChild(li);
+import bootCamper from './data/data.json' with {type: 'json'};
 
-const bootCampers = [
-	"Frodo",
-	"Bilbo",
-	"Aragorn",
-	"Legolas",
-	"Gimli",
-	"Samwise",
-	"Gandalf",
-	"Elrond",
-	"Galadriel",
-	"Arwen",
-	"Thranduil",
-	"Thorin",
-	"Balin",
-	"Bard",
-	"Beorn",
-	"Radagast",
-	"Thrain",
-	"Thror",
-	"Tom Bombadil",
-	// add more boot campers here
-];
+const chosen = [];
 
-unpicked.onclick = () => {
-	if (bootCampers.length > 0) {
-		const index = Math.floor(Math.random() * bootCampers.length);
-		const removed = bootCampers.splice(index, 1)[0];
-		safeCampers.push(removed);
-		myH1.textContent = "Picking from section 1";
-		li.textContent = removed;
-	} else {
-		myH1.textContent = "Section 1 empty";
-	}
-};
+const title = document.createElement("h1");
+const wrapper = document.getElementById("wrapper")
+const randomizer = document.getElementById('randomizer');
 
-picked.onclick = () => {
-	if (safeCampers.length > 0) {
-		const index = Math.floor(Math.random() * safeCampers.length);
-		const removed = safeCampers.splice(index, 1)[0];
-		bootCampers.push(removed);
-		myH1.textContent = "Picking from section 2";
-		li.textContent = removed;
-	} else {
-		myH1.textContent = "Section 2 empty";
-	}
-};
 
-const object = { ...bootCampers };
-console.log(object);
+function assignRandomBootCamper() {
+    const randNumb = Math.floor(Math.random() * bootCamper.length);
+    if (bootCamper.length > 0) {
+        const chosenBC = bootCamper[randNumb];
+        chosen.push(chosenBC);
+        bootCamper.splice(randNumb, 1);
+        title.textContent = chosenBC;
+        console.log(bootCamper);
+    } else {
+        title.textContent = "All gone...";
+    }
+}
+
+randomizer.addEventListener('click', assignRandomBootCamper);
+wrapper.appendChild(title);
